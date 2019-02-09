90a00a567d7a0460ebe7c3b6f129efdb96e4e31f
tatin <sergio.detter@outlook.com>
2019-02-08 21:29:49 -0300

    resuelto ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,20 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var divisor;
+	var dividendo;
+	var cociente;
+	var resto;
+
+	divisor=numeroDivisor.value;
+	dividendo=numeroDividendo.value;
+
+	divisor=parseInt(divisor);
+	dividendo=parseInt(dividendo);
+
+	cociente=divisor/dividendo;
+	resto=divisor%dividendo;
+
+	alert("El cociente es: "+cociente+" y el resto seria: "+resto);
+
 }
