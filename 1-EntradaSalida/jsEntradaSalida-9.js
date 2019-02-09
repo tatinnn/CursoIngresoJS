96d375cc5866db6e0b171428da993efdee8768c9
tatin <sergio.detter@outlook.com>
2019-02-08 22:29:23 -0300

    ej 9 semi resuelto

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,21 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueld;
+	var resultad;
+	var aumento;
+
+	sueld=parseInt(sueld);
+	resultad=parseInt(resultad);
+	aumento=parseInt(aumento);
+
+	sueld=sueldo.value;
+
+    aumento=sueld*10/100;
+
+	resultad=aumento+sueld;
+
+	resultado.value=resultad;
 
 	
 }
