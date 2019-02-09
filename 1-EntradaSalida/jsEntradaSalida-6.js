e544fad76b0e06eec560ed2167508395292d10fb
tatin <sergio.detter@outlook.com>
2019-02-08 21:02:28 -0300

    resueltos ejercicios 5 y 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,21 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+
+	primerNumero=numeroUno.value;
+	segundoNumero=numeroDos.value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+	
+
+	suma=primerNumero+segundoNumero;
+
+	alert("El resultado es: "+suma);	
+	
 
 }
 
